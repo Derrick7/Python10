c4ff6be4804d45dd030d6971a9b293ba27696422
Derrick7 <derrick.j.m@hotmail.co.uk>
2016-03-11 10:48:36 +0200

    Added files via upload

---
+++ sectio a pre release.py
@@ -0,0 +1,41 @@
+a=[]
+#a is the array in which temperatures are stored
+high=0
+low=100
+
+
+#def highest():
+
+
+#def lowest():
+
+
+#def difference():
+
+
+
+
+for i in range(0,3):
+    temperature=float(input('enter babys temperature'))
+    a.append(temperature)
+
+for i in range(0,len(a)):
+        if i>high:
+          high==i
+   # return highest
+
+for i in range(0,len(a)):
+      if i<low:
+        low==i
+    #return lowest
+
+dif=(high-low)
+  #return difference
+
+
+#b=highest
+#c=lowest
+#d=difference
+
+print('highest',high,'and lowest',low,'and the difference',dif)
+
